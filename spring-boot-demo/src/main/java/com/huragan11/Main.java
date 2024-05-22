e362bb34e04019720635b75b1d7d1048f6075d9f
s22451 <127113762+s22451@users.noreply.github.com>
2024-05-22 17:53:46 +0200

    Clean up project files

--- spring-boot-demo/src/main/java/com/huragan11/Main.java
+++ spring-boot-demo/src/main/java/com/huragan11/Main.java
@@ -2,12 +2,10 @@ package com.huragan11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class SpringBootDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
 }
