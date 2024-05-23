8cf64ac3eeca2f9be1fbb545729233580e6bbf79
s22451 <127113762+s22451@users.noreply.github.com>
2024-05-23 19:31:20 +0200

    Update Customer.java
    Removed unused methods. Getters and Setters are now used by @Data annotation

--- spring-boot-demo/src/main/java/com/huragan11/Customer.java
+++ spring-boot-demo/src/main/java/com/huragan11/Customer.java
@@ -1,10 +1,12 @@
 package com.huragan11;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Objects;
 
 @Entity
+@Data
 public class Customer {
     @Id
     @SequenceGenerator(
@@ -16,12 +18,15 @@ public class Customer {
             strategy = GenerationType.SEQUENCE,
             generator = "customer_id_sequence"
     )
-    private Integer id;
+    private long id;
     private String name;
     private String email;
     private Integer age;
 
-    public Customer(Integer id, String name, String email, Integer age) {
+    public Customer(long id, String name, String email, Integer age) {
+        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")){
+            throw new IllegalArgumentException();
+        }
         this.id = id;
         this.name = name;
         this.email = email;
@@ -31,49 +36,11 @@ public class Customer {
     public Customer() {
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
+        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")){
+            throw new IllegalArgumentException();
+        }
         this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, age);
     }
 
     @Override
