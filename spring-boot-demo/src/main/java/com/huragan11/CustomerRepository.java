6f2e116f914066eb8c295936f83ef2f9b5ba7ee1
s22451 <127113762+s22451@users.noreply.github.com>
2024-05-23 19:34:40 +0200

    Clean up
    Removed unused imports and useless annotations

--- spring-boot-demo/src/main/java/com/huragan11/CustomerRepository.java
+++ spring-boot-demo/src/main/java/com/huragan11/CustomerRepository.java
@@ -3,6 +3,5 @@ package com.huragan11;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
--- spring-boot-demo/src/main/java/com/huragan11/Main.java
+++ spring-boot-demo/src/main/java/com/huragan11/Main.java
@@ -2,10 +2,6 @@ package com.huragan11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 
