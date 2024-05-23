56ec2b4c4949f2eb776def8eae88c979e6b3c76b
s22451 <127113762+s22451@users.noreply.github.com>
2024-05-23 09:38:14 +0200

    Update Main, temp. GET method

--- spring-boot-demo/src/main/java/com/huragan11/Main.java
+++ spring-boot-demo/src/main/java/com/huragan11/Main.java
@@ -2,10 +2,31 @@ package com.huragan11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        return new GreetResponse(
+                "well hello there",
+                List.of("C++","Java","C#"),
+                new Person("Alex", 23,500.0)
+                );
+    }
+
+    record Person(String name, int age, double savings){}
+    record GreetResponse(
+            String greet,
+            List<String> favProgLang,
+            Person person
+    ){}
 }
